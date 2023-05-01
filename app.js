9e04b5aff6a05413bba05c2a13e07a2e18faa489
chaevid <chaevid@gmail.com>
2023-05-01 14:19:44 +0900

    Set default background image if API request fails

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const UNSPLASH_API_KEY = process.env.UNSPLASH_API_KEY;
-const OPENWEATHERMAP_API_KEY = process.env.OPENWEATHERMAP_API_KEY;
+const UNSPLASH_API_KEY = 'DYWZDzYyj9BIU9ezT9j00UG8sqjm7KdOzxUQAxoueFs';
+const OPENWEATHERMAP_API_KEY = 'ba0a7ac8b4aa1d89506ec1b502dd04cb';
 
 // Clock
 function updateTime() {
@@ -146,6 +146,9 @@ function getWeather(latitude, longitude) {
     })
     .catch((error) => {
       console.error('Error fetching weather data:', error);
+      document.querySelector(
+        '.background'
+      ).style.backgroundImage = `url('/src/bg-image-unsplash.jpg')`;
     });
 }
 
